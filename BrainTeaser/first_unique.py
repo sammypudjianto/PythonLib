f558a32bd4da24845f4e411fec084ac64c270307
sammypudjianto <sammy.pudjianto@gmail.com>
2021-03-19 07:38:41 +0000

    add 2 files

---
+++ BrainTeaser/first_unique.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+"""
+Design an algorithm to find the first unique element in an array.
+"""
---
+++ BrainTeaser/rabbitsteps.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+"""
+ rabbit wants to climb some stairs and it can do steps of 1 or 2.
+ How many possible paths are there to follow (
+ e.g 1-1-1... or 2-2-2 ... or 2-1-2-1... etc)
+"""
+
+
+def count_no_steps(totsteps: int) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    pass
